90deeb51c685f637a6675d8679575189a2d18d97
Andy Birenzi <andybirenzi@andys-mbp.home>
2020-07-09 14:40:14 -0400

    updated Homescreen to pass a short parameter

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Text, StyleSheet, Button, View, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
-  console.log(props);
+const HomeScreen = ({navigation}) => {
+  // console.log(props);
   return (
     <View>
       <Text style={styles.text}>Hey Andy !</Text>
       <Button title="Go to component Demo" 
-      onPress={()=>props.navigation.navigate('Components')}/>
-      <TouchableOpacity onPress={()=> props.navigation.navigate('Components')}>
+      onPress={()=>navigation.navigate('Components')}/>
+      <TouchableOpacity onPress={()=> navigation.navigate('Components')}>
         <Text>Go to list demo</Text>
       </TouchableOpacity>
     </View>

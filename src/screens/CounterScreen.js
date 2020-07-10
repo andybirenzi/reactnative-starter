dff14ccb5343d8af904c46f291e8f04dad3a610f
Andy Birenzi <andybirenzi@andys-mbp.home>
2020-07-10 15:37:54 -0400

    added reducer to counterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState,useReducer} from 'react';
 import {Text, View, StyleSheet,Button} from 'react-native';
 
+const reducer = (state,action)=>{
+    switch(action.type){
+        case 'Increase':
+            return {...state,counter: state.counter+action.payload};
+        case 'Decrease':
+            return {...state,counter: state.counter-action.payload};
+        default:
+    }
+
+}
+
 const CounterScreen= () =>{
-    const [counter,setCounter]=useState(0);
+    // const [counter,setCounter]=useState(0);
+    const [state,myReducer]=useReducer(reducer,{counter:0});
     return (
         <View>
-            <Button title="Increase" onPress={()=>{
-              setCounter(counter+1);
-            }}/>
-            <Button title="Decrease" onPress={()=>{
-              setCounter(counter-1);
-            }}/>
-            <Text>{counter}</Text>
+            <Button title="Increase" onPress={()=>myReducer({type:"Increase",payload:1})}/>
+            <Button title="Decrease" onPress={()=>
+            // {
+            //   setCounter(counter-1);
+            // }
+            myReducer({type:"Decrease",payload:1})
+            }/>
+            <Text>{state.counter}</Text>
         </View>
 
     );
